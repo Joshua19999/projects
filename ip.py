36284a1170166c9b9b39c42b6fc5b2725a3e9af8
Joshua19999 <87553901+Joshua19999@users.noreply.github.com>
2025-10-13 13:33:50 -0600

    Delete ip.py

--- ip.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def get_ip_address():
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    return ip_address
-
-print("La dirección IP del servidor es:", get_ip_address())
